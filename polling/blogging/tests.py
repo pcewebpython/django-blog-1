b6b21163509726b639017ccc64113696e974f1e4
Tom <tsulgrove@gmail.com>
2020-05-21 12:07:33 -0700

    copy comp assign from local dir

--- polling/blogging/tests.py
+++ polling/blogging/tests.py
@@ -1,15 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-
 from blogging.models import Post
-
 
 class PostTestCase(TestCase):
     fixtures = ['blogging_test_fixture.json', ]
 
     def setUp(self):
         self.user = User.objects.get(pk=1)
-
+        
     def test_string_representation(self):
         expected = "This is a title"
         p1 = Post(title=expected)
